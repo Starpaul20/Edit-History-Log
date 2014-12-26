7bd6982441257b6cdf7c607c873d4cdd699f6483
PaulBender <paulbender@live.com>
2014-12-26 14:29:53 -0500

    Added codename

--- inc/plugins/edithistory.php
+++ inc/plugins/edithistory.php
@@ -51,6 +51,7 @@ function edithistory_info()
 		"author"			=> "Starpaul20",
 		"authorsite"		=> "http://galaxiesrealm.com/index.php",
 		"version"			=> "1.0",
+		"codename"			=> "edithistory",
 		"compatibility"		=> "18*"
 	);
 }
