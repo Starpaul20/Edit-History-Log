211ee895cd5722151d2af65b13254d22f9641e39
PaulBender <paulbender@live.com>
2016-08-03 14:26:51 -0400

    Bump to version v1.3

--- inc/plugins/edithistory.php
+++ inc/plugins/edithistory.php
@@ -53,7 +53,7 @@ function edithistory_info()
 		"website"			=> "http://galaxiesrealm.com/index.php",
 		"author"			=> "Starpaul20",
 		"authorsite"		=> "http://galaxiesrealm.com/index.php",
-		"version"			=> "1.2",
+		"version"			=> "1.3",
 		"codename"			=> "edithistory",
 		"compatibility"		=> "18*"
 	);
